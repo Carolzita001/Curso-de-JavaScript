970e245310c567eae5ac7db5ba8011c0fd646624
Ana Carolina T. Lino <caroltex2004@gmail.com>
2022-04-21 15:42:26 -0300

    Aprendendo: estruturas de repetição.

---
+++ aula13/ambiente.js
@@ -0,0 +1,26 @@
+var c = 1
+
+do{
+    console.log(`Tarefa ${c}`)
+    c++
+}while(c <= 6)// Estrutura de repetição com teste lógico no ínicio.
+
+/*
+while( c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}// Estrutura de repetição com teste lógico no início.
+
+/*
+while(c <= 6){
+    console.log('Boa tarde!')
+    c++
+}*///deste modo se está usando o raiocínio lógico!
+
+/*
+console.log('Boa Tarde!')
+console.log('Boa Tarde!')
+console.log('Boa Tarde!')
+console.log('Boa Tarde!')
+console.log('Boa Tarde!')
+console.log('Boa Tarde!')*///desse modo se está utilizando da força pruta.
